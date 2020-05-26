cc677f7fb38fb4f0344e928ec09216f4b155aead
Yuliya Mihaylova <yulu.2000@hotmail.com>
2020-05-26 15:02:05 +0300

    for deleting .idea files

--- chatapp/src/main/java/app/repositories/UsersInRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repositories;
-
-import app.models.UsersInRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersInRoomRepository extends JpaRepository<UsersInRoom, Long> {
-}
